4bb3126790d46a799e5e0ecb9c260f14c7844e8a
Blackbetty312 <rubel8@o2.pl>
2019-11-06 22:12:35 +0100

    adding echohandler and connectTo function

--- src/main/java/com/blackbetty/AudioEcho.java
+++ src/main/java/com/blackbetty/AudioEcho.java
@@ -1,10 +1,12 @@
 package com.blackbetty;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class AudioEcho extends ListenerAdapter {
 
@@ -14,6 +16,18 @@ public class AudioEcho extends ListenerAdapter {
         Member member = event.getMember();
         GuildVoiceState voiceState = member.getVoiceState();
         VoiceChannel voiceChannel = voiceState.getChannel();
-        // comment for nothing xD
+
+
+
+    }
+
+    private void connectTo(VoiceChannel channel) {
+        Guild guild = channel.getGuild();
+        AudioManager audioManager = guild.getAudioManager();
+        EchoHandler echoHandler = new EchoHandler();
+
+        audioManager.setSendingHandler(echoHandler);
+        audioManager.setReceivingHandler(echoHandler);
+        audioManager.openAudioConnection(channel);
     }
 }
---
+++ src/main/java/com/blackbetty/EchoHandler.java
@@ -0,0 +1,48 @@
+package com.blackbetty;
+
+import net.dv8tion.jda.api.audio.*;
+import net.dv8tion.jda.api.entities.User;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.nio.ByteBuffer;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class EchoHandler implements AudioSendHandler, AudioReceiveHandler {
+
+    private final Queue<byte[]> queue = new ConcurrentLinkedQueue<>();
+
+    /*  receive handler     */
+    @Override
+    public boolean canReceiveCombined() {
+        return queue.size() < 10;
+    }
+
+    @Override
+    public void handleCombinedAudio(@Nonnull CombinedAudio combinedAudio) {
+        if (combinedAudio.getUsers().isEmpty())
+            return;
+
+        byte[] data = combinedAudio.getAudioData(1.0f); // volume at 100% = 1.0 (50% = 0.5 / 55% = 0.55)
+        queue.add(data);
+    }
+
+    /*      send handler    */
+    @Override
+    public boolean canProvide() {
+        return !queue.isEmpty();
+    }
+
+    @Nullable
+    @Override
+    public ByteBuffer provide20MsAudio() {
+        byte[] data = queue.poll();
+        return data == null ? null : ByteBuffer.wrap(data);
+    }
+
+    @Override
+    public boolean isOpus() {
+        return false;
+    }
+}
